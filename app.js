dce0a08f39ad5d85c4a15fcf2330839fc5dfc7f7
Chechu <chechu@Chechus-MacBook-Pro-2.local>
2020-02-02 16:10:33 +0100

    secret fix

--- app.js
+++ app.js
@@ -23,10 +23,7 @@ function main () {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
   });
-  app.use(bodyParser.json());
-  app.use("/api/users", users);
-  app.use("/api/backend", backend);
-  app.use("/api/management", management);
+  
   app.use("/api/management", courses);
 
   const db = require("./config/keys").mongoURI;
